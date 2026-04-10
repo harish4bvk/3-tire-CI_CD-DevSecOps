ca6b884310b1fb317496ab791b383822c035fc82
harish bodapati <harish4bvk@gmail.com>
2026-04-10 21:47:47 +0530

    update ci-cd

--- Terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "harish-terraform-state-dev-jerney"
-    key            = "eks/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"
-  }
-}
